a75cc6064055f8fb73c96261e9bf7d2ebbccda9e
Pavel Maslov <pavel.maslov@tretton37.com>
2021-03-11 16:18:58 +0100

    jar + readme

--- src/main/kotlin/tech/maslick/camt053/Helloworld.kt
+++ src/main/kotlin/tech/maslick/camt053/Helloworld.kt
@@ -1,3 +1,5 @@
+package tech.maslick.camt053
+
 import net.tjeerd.camt053parser.model.BankToCustomerStatementV02
 import net.tjeerd.camt053parser.model.Document
 import net.tjeerd.camt053parser.model.GroupHeader42
@@ -9,17 +11,13 @@ import javax.xml.bind.Marshaller
 fun main() {
     val document = Document()
     val statementV02 = BankToCustomerStatementV02()
-
     statementV02.grpHdr = GroupHeader42()
     document.bkToCstmrStmt = statementV02
 
-    val file = File("helloworld.xml")
-    val jaxbContext: JAXBContext = JAXBContext.newInstance(Document::class.java)
-
+    val jaxbContext = JAXBContext.newInstance(Document::class.java)
     val jaxbMarshaller = jaxbContext.createMarshaller()
     jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
 
-    jaxbMarshaller.marshal(document, file);
+    jaxbMarshaller.marshal(document, File("helloworld.xml"));
     jaxbMarshaller.marshal(document, System.out);
-
 }
