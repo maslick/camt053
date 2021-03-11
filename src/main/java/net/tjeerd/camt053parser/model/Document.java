50525a3b620da05eeaa3edcf5555c01b1406dc72
Pavel Maslov <pavel.maslov@tretton37.com>
2021-03-11 15:41:36 +0100

    xml root element

--- src/main/java/net/tjeerd/camt053parser/model/Document.java
+++ src/main/java/net/tjeerd/camt053parser/model/Document.java
@@ -12,6 +12,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 
 /**
@@ -37,6 +38,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "Document", propOrder = {
     "bkToCstmrStmt"
 })
+@XmlRootElement
 public class Document {
 
     @XmlElement(name = "BkToCstmrStmt", required = true)
